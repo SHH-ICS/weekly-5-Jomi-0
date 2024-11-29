8ef39e2e77fbdc97c33987152146c45fba97ca23
Jomi O <78754002+Jomi-0@users.noreply.github.com>
2024-11-29 15:37:18 +0000

    assignment 5

--- handler.php
+++ handler.php
@@ -4,38 +4,31 @@
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-  <title>Pizza Price Calculation</title>
+  <title></title>
 </head>
 
 <body>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Pizza Price</span>
+        <span class="mdl-layout-title"></span>
         <div class="mdl-layout-spacer"></div>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="page-content" style="text-align: center; margin-top: 50px;">
         <?php
-        if (isset($_POST['pizzasize']) && isset($_POST['toppings'])) {
-          $pizzasize = escapeshellarg($_POST['pizzasize']);
-          $toppings = intval($_POST['toppings']);
+        if (isset($_POST['digits'])) {
+          $digits = intval($_POST['digits']); 
 
-          $output = shell_exec("python3 assignment.py $pizzasize $toppings");
-
-          echo "<p>The total price of your pizza is: $" . htmlspecialchars($output) . "</p>";
+          if ($digits > 0) {
+            $output = shell_exec("python3 assignment.py " . escapeshellarg($digits)); 
+            echo "<p>Approximation of Pi with $digits terms: " . htmlspecialchars($output) . "</p>"; 
+          } else {
+            echo "<p>Error: Please enter a positive integer.</p>";
+          }
         } else {
-          echo "<p>Error: Please select a pizza size and the number of toppings.</p>";
+          echo "<p>Error: No input received.</p>";
         }
-        ?> <a href="javascript:history.back()" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-          Go Back
-        </a>
-        <a href="Thankyou.html" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">Proceed</a>
-
-      </div>
-    </main>
-  </div>
-</body>
-
-</html>
+        ?>
+<html>
